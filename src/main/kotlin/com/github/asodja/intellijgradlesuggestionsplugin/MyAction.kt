eb6f95dcd90f349a765c12fd2fca599e91828d1e
GitHub Action <action@github.com>
2023-07-28 09:00:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/asodja/intellijgradlesuggestionsplugin/MyAction.kt
+++ src/main/kotlin/com/github/asodja/intellijgradlesuggestionsplugin/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.asodja.intellijgradlesuggestionsplugin
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
--- src/main/kotlin/com/github/asodja/intellijgradlesuggestionsplugin/MyBundle.kt
+++ src/main/kotlin/com/github/asodja/intellijgradlesuggestionsplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.asodja.intellijgradlesuggestionsplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
